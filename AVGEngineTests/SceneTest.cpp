2d21ffaecdb0c6e8af5103e35786391f429be68e
GM2000 <GM2000@outlook.com>
2018-02-10 16:07:18 +0800

    增加表达式测试

--- AVGEngineTests/SceneTest.cpp
+++ AVGEngineTests/SceneTest.cpp
@@ -1,9 +1,12 @@
+#include <comdef.h> 
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "../AVGEngine/src/scene/Scene.h"
 #include "../AVGEngine/src/util/Resource.h"
 #include "../AVGEngine/src/scene/Label.h"
 #include "../AVGEngine/src/scene/CustomWidget.h"
+
+#define EPSINON 0.0001
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -73,9 +76,33 @@ namespace avg_engine_tests
 
 		void checkWidgetContainer() const
 		{
-			const auto label1 = findWidget<CustomWidget>("label1");
+			const auto label1 = findWidget<Label>("label1");
+			const auto dialog1 = findWidget<CustomWidget>("dialog1");
 
-			if (label1->getString("caption") != "Label 1" )
+			if (label1->getString("caption") != "Label 1")
+				Assert::Fail(L"Failed to set label data");
+
+			if (dialog1->findWidget<CustomWidget>("dialog1")->findWidget<Label>("dialogLabel1")->getString("caption") != "Test!")
+				Assert::Fail(L"Failed to set label data");
+
+			if (dialog1->findWidget<CustomWidget>("dialog1")->findWidget<Label>("dialogLabel2")->getString("caption") != "Value 2")
+				Assert::Fail(L"Failed to set label data");
+		}
+	};
+	class TestScene3 : protected Scene
+	{
+	public:
+		TestScene3()
+		{
+			Scene::init(*Resource::getConfig("res.scene.TestScene3.cnf"));
+		}
+
+		void checkWidgetContainer() const
+		{
+			const auto dialog1 = findWidget<CustomWidget>("dialog1");
+			const auto height = dialog1->findWidget<Label>("label1")->getDouble("size.height");
+
+			if (height != (1.0 + 2.0 + 3.0 * (1.0 + 2.0 + 3.0 / 3.0 - 10.0) / 10.0 - 1.0))
 				Assert::Fail(L"Failed to set label data");
 		}
 	};
@@ -102,6 +129,41 @@ namespace avg_engine_tests
 		{
 			Resource::init();
 			TestScene2 testScene;
+			testScene.checkWidgetContainer();
+		}
+
+		TEST_METHOD(expressionTest)
+		{
+			//在Scene外测试
+			std::vector<std::pair<std::string, double>> testData
+			{
+				std::make_pair("1 + 1", 1.0 + 1.0),
+				std::make_pair("1 + 3 * 8", 1.0 + 3.0 * 8.0),
+				std::make_pair("1 + 3 * 8 / 4", 1.0 + 3.0 * 8.0 / 4.0),
+				std::make_pair("1 + 3 * 8 / 4 - 1", 1.0 + 3.0 * 8.0 / 4.0 - 1.0),
+				std::make_pair("1 + 3 * 8 / (4 - 1)", 1.0 + 3.0 * 8.0 / (4.0 - 1.0)),
+				std::make_pair("1 + 3 * (8 / (4 - 1.2))", 1.0 + 3.0 * (8.0 / (4.0 - 1.2))),
+				std::make_pair("1 + 3 - 1 * (8 / (4 - 1))", 1.0 + 3.0 - 1.0 * (8.0 / (4.0 - 1.0))),
+				std::make_pair("1 + (3 - 1) * (8 / (4 - 1))", 1.0 + (3.0 - 1.0) * (8.0 / (4.0 - 1.0))),
+				std::make_pair("1+(3-1)*(8/(4-1))", 1.0 + (3.0 - 1.0 )* (8.0 / (4.0 - 1.0))),
+				std::make_pair("1  +(3  -  1  )  *  ( 8/  ( 4-1 )    )", 1.0 + (3.0 - 1.0) * (8.0 / (4.0 - 1.0))),
+			};
+
+			for (auto& data : testData)
+			{
+				const auto result = Config::getExpressionResult(data.first);
+				if (abs(result - data.second) > EPSINON)
+				{
+					const _bstr_t t = data.first.c_str();
+					const auto pwchar = static_cast<wchar_t*>(t);
+					const std::wstring wStr = pwchar;
+					Assert::Fail((std::wstring(L"Failed to calcuate ") + wStr).c_str());
+				}
+			}
+
+			//在Scene内测试
+			Resource::init();
+			TestScene3 testScene;
 			testScene.checkWidgetContainer();
 		}
 	};
